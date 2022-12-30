de616bbdb4c14d2c6a1c17104001ee0e66c5ecf4
kimsooyoung <tge1375@naver.com>
2022-12-30 16:36:22 +0900

    first commit

---
+++ py_topic_tutorial/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'py_topic_tutorial'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kimsooyoung',
+    maintainer_email='tge1375@naver.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'topic_pub_node = py_topic_tutorial.topic_example_1_publisher:main',
+            'topic_sub_node = py_topic_tutorial.topic_example_2_subscriber:main',
+            'mimic_node     = py_topic_tutorial.topic_example_4_mimic:main',
+        ],
+    },
+)
---
+++ setup_scripts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+sudo apt-get update
+
+echo -e "${GREEN}==== Installing External ROS Packages ====${NC}"
+
+sudo apt install ros-foxy-xacro -y
+sudo apt install ros-foxy-joint-state-publisher-gui -y
+sudo apt install ros-foxy-joint-state-publisher -y
+sudo apt install ros-foxy-robot-state-publisher -y
+sudo apt install ros-foxy-nav2-* -y
+sudo apt install ros-foxy-slam-toolbox -y
+sudo apt install ros-foxy-teleop-twist-keyboard -y
+sudo apt install ros-foxy-gazebo-ros* -y
+sudo apt install ros-foxy-ros2-controllers -y
+sudo apt install ros-foxy-ros2-control -y
+sudo apt install ros-foxy-rqt-robot-steering -y
+sudo apt install ros-foxy-controller-manager -y
+sudo apt install ros-foxy-controller-manager-msgs -y
+# sudo apt install ros-foxy-gazebo-ros2-control -y
+sudo apt install ros-foxy-gazebo-ros2-control-demos -y
