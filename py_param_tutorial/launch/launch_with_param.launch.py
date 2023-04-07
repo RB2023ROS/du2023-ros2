e58de7ee825a0ce84f78761ff6b2af5ffa99bea1
kimsooyoung <tge1375@naver.com>
2023-04-07 18:57:50 +0900

    param with namespace

--- py_param_tutorial/launch/launch_with_param.launch.py
+++ py_param_tutorial/launch/launch_with_param.launch.py
@@ -1,6 +1,5 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -16,19 +15,6 @@ def generate_launch_description():
             {'int_param': 112},
         ],
     )
-
-    # config = os.path.join(
-    #     get_package_share_directory('py_param_tutorial'), 'config', 'params.yaml'
-    # )
-    
-    # param_ex_node = Node(
-    #     package = 'py_param_tutorial',
-    #     executable = 'param_example',
-    #     name = 'param_example',
-    #     output='screen',
-    #     parameters = [config]
-    # )
-
 
     return LaunchDescription([
         param_ex_node
---
+++ py_param_tutorial/launch/launch_with_yaml.launch.py
@@ -0,0 +1,23 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    config = os.path.join(
+        get_package_share_directory('py_param_tutorial'), 'config', 'params.yaml'
+    )
+    
+    param_ex_node = Node(
+        package = 'py_param_tutorial',
+        executable = 'param_example',
+        name = 'param_example',
+        output='screen',
+        parameters = [config]
+    )
+
+    return LaunchDescription([
+        param_ex_node
+    ])
