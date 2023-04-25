f2625bda92552a9b6e424423d94694ddf70fef65
kimsooyoung <tge1375@naver.com>
2023-04-26 00:56:40 +0900

    Ongoing : composition

---
+++ composition_tutorial/launch/test.launch.py
@@ -0,0 +1,21 @@
+import launch
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+def generate_launch_description():
+    """Generate launch description with multiple components."""
+    container = ComposableNodeContainer(
+            name='my_container',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='lc_components',
+                    plugin='lc_components::MoveRobot',
+                    name='moverobot'),
+            ],
+            output='screen',
+    )
+    
+    return launch.LaunchDescription([container])
