931ba3b5b92c9c46fda1fed4dc8b2b3eac1e01e6
kimsooyoung <tge1375@naver.com>
2023-04-29 16:20:20 +0900

    pointnet node confirm

--- pointnet_torch/pointnet_torch/pointnet_node.py
+++ pointnet_torch/pointnet_torch/pointnet_node.py
@@ -1,45 +1,20 @@
-import os
-import random
-from random import randrange
-# from IPython.display import clear_output
 import numpy as np
+
 import torch
-import torch.nn as nn
 import torch.nn.parallel
-import torch.optim as optim
 import torch.utils.data
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-import torchvision.utils as vutils
 from torch.autograd import Variable
-from datasets import PartDataset
-from pointnet import PointNetCls
-import torch.nn.functional as F
-import matplotlib.pyplot as plt
-import open3d as o3
-import download
 
-
+from .pointnet import PointNetCls
 
 if torch.cuda.is_available():
     import torch.backends.cudnn as cudnn
 
-# General parameters
-NUM_POINTS = 10000
-
-# TODO: rclpy param
-MODEL_PATH = '/home/kimsooyoung/Downloads/pointnet/pointnet.pytorch/utils/cls/cls_model_23.pth'
-DATA_FOLDER = '/home/kimsooyoung/Downloads/pointnet/pointnet.pytorch/shapenetcore_partanno_segmentation_benchmark_v0'
-
-# download dataset and pre-trained model
-# download.download_contents()
-
-# Create dataset object
-test_dataset_seg = PartDataset(
-    root=DATA_FOLDER,
-    train=False,
-    classification=False,
-    npoints=NUM_POINTS)
+# RCLPY packages
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import PointCloud2
+from sensor_msgs_py import point_cloud2 as pc2
 
 # Problem ontology
 classes_dict = {'Airplane': 0, 'Bag': 1, 'Cap': 2, 'Car': 3, 'Chair': 4, 
@@ -47,87 +22,79 @@ classes_dict = {'Airplane': 0, 'Bag': 1, 'Cap': 2, 'Car': 3, 'Chair': 4,
                 'Motorbike': 10, 'Mug': 11, 'Pistol': 12, 'Rocket': 13, 
                 'Skateboard': 14, 'Table': 15}
 
-# Create the classification network from pre-trained model
-classifier = PointNetCls(k=len(classes_dict.items()), num_points=NUM_POINTS)
-if torch.cuda.is_available():
-    classifier.cuda()
-    classifier.load_state_dict(torch.load(MODEL_PATH))
-else:
-    classifier.load_state_dict(torch.load(MODEL_PATH, map_location='cpu'))
-classifier.eval()
+class PointNetNode(Node):
 
-# Simple point cloud coloring mapping
-def read_pointnet_colors(seg_labels):
-    map_label_to_rgb = {
-        1: [0, 255, 0],
-        2: [0, 0, 255],
-        3: [255, 0, 0],
-        4: [255, 0, 255],  # purple
-        5: [0, 255, 255],  # cyan
-        6: [255, 255, 0],  # yellow
-    }
-    colors = np.array([map_label_to_rgb[label] for label in seg_labels])
-    return colors
+    def __init__(self):
+        super().__init__('pointnet_cls_node')
 
-# Three.js based visualizer
-# visualizer = JVisualizer()
+        self.declare_parameter('model_path', '/home/kimsooyoung/Downloads/pointnet/pointnet.pytorch/utils/cls/cls_model_23.pth')
+        self.declare_parameter('num_points', 10000)
 
-# Basic inference and visualization loop
-MAX_SAMPLES = 15
-for samples in range(MAX_SAMPLES):
-    random_index = randrange(len(test_dataset_seg))
-    print('[Sample {} / {}]'.format(random_index, len(test_dataset_seg)))
-    
-    # # clean visualization
-    # visualizer.clear()
-    # clear_output()
-    
-    # TODO: Point Set from ROS 2
-    # # 1. Open3D to numpy
-    # point_set = np.asarray(
-    #     open3d.io.read_point_cloud(fn[1], format='xyz').points,
-    #     dtype=np.float32)
-    #
-    # # 2. numpy resample
-    # choice = np.random.choice(len(seg), self.npoints, replace=True)
-    # point_set = point_set[choice, :]
-    # seg = seg[choice]
-    #
-    # # 3. numpy to torch
-    # point_set = torch.from_numpy(point_set)
-    # seg = torch.from_numpy(seg)
-    # cls = torch.from_numpy(np.array([cls]).astype(np.int64))
+        MODEL_PATH = self.get_parameter('model_path').value
+        NUM_POINTS = self.get_parameter('num_points').value
 
-    # get next sample
-    point_set, seg = test_dataset_seg.__getitem__(random_index)
+        # Create the classification network from pre-trained model
+        self.classifier = PointNetCls(k=len(classes_dict.items()), num_points=NUM_POINTS)
+        if torch.cuda.is_available():
+            self.classifier.cuda()
+            self.classifier.load_state_dict(torch.load(MODEL_PATH))
+        else:
+            self.classifier.load_state_dict(torch.load(MODEL_PATH, map_location='cpu'))
+        self.classifier.eval()
 
-    # create cloud for visualization
-    cloud = o3.geometry.PointCloud()
-    cloud.points = o3.utility.Vector3dVector(point_set)
-    cloud.colors = o3.utility.Vector3dVector(read_pointnet_colors(seg.numpy()))
+        self.get_logger().info('PointNet classification network has been loaded')
 
-    # 4. perform inference in GPU
-    points = Variable(point_set.unsqueeze(0))
-    points = points.transpose(2, 1)
-    if torch.cuda.is_available():
-        points = points.cuda()
-    pred_logsoft, _ = classifier(points)
+        self.pc_subscription = self.create_subscription(
+            PointCloud2, 'pointcloud',
+            self.obj_callback, 10
+        )
 
-    # move data back to cpu for visualization
-    pred_logsoft_cpu = pred_logsoft.data.cpu().numpy().squeeze()
-    pred_soft_cpu = np.exp(pred_logsoft_cpu)
-    pred_class = np.argmax(pred_soft_cpu)
-    # list(classes_dict.keys())[pred_class]
-    # rclpy.logger()
-    
-    # Visualize probabilities
-    plt.xticks(list(classes_dict.values()), list(classes_dict.keys()),rotation=90)
-    plt.xlabel('Classes')
-    plt.ylabel('Probabilities')
-    plt.plot(list(classes_dict.values()), pred_soft_cpu)
-    plt.show()
+        self.get_logger().info('PointNet classification Node has been started')
 
-    o3.visualization.draw_geometries([cloud])
+        self.cb_flag = True
 
-    input('Your object is a [{}] with probability {:0.3}. Press enter to continue!'
-          .format(list(classes_dict.keys())[pred_class], pred_soft_cpu[pred_class]))
+    def obj_callback(self, msg):
+        # 1. Convert PointCloud2 message to numpy array
+        points_list = []
+        for data in pc2.read_points(msg, skip_nans=True):
+            points_list.append([data[0], data[1], data[2]])
+        points = np.asarray(points_list, dtype=np.float32)
+
+        # 2. numpy resample
+        m, n = points.shape
+        choice = np.random.choice(m, 10000, replace=True)
+        point_set = points[choice, :]
+
+        # 3. numpy to torch
+        point_set = torch.from_numpy(point_set)
+
+        # 4. perform inference in GPU
+        if self.cb_flag == True:
+            points = Variable(point_set.unsqueeze(0))
+            points = points.transpose(2, 1)
+            if torch.cuda.is_available():
+                points = points.cuda()
+            pred_logsoft, _ = self.classifier(points)
+
+            # move data back to cpu 
+            pred_logsoft_cpu = pred_logsoft.data.cpu().numpy().squeeze()
+            pred_soft_cpu = np.exp(pred_logsoft_cpu)
+            pred_class = np.argmax(pred_soft_cpu)
+
+            self.get_logger().info(f'Your object is a [{list(classes_dict.keys())[pred_class]}]')
+            self.get_logger().info('Probability is a [{:0.3}]'.format(pred_soft_cpu[pred_class]))
+        
+            self.cb_flag = False
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = PointNetNode()
+    rclpy.spin(node)
+    node.destroy_node()
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- pointnet_torch/setup.py
+++ pointnet_torch/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'pointnet_torch    = pointnet_torch.pointnet_node:main',
         ],
     },
 )
